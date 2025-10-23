26b9784666f65e59e5f724bdc6c9e8d66a22acf2
Himanshu <76879328+himan-shuu@users.noreply.github.com>
2025-10-23 18:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import re
 from intents_responses import get_response
 
 # Load model & vectorizer
-clf = pickle.load(open("model/logistic.pkl", "rb"))
+logistic = pickle.load(open("model/logistic.pkl", "rb"))
 vectorizer = pickle.load(open("model/vectorizer.pkl", "rb"))
 
 # Preprocess function
